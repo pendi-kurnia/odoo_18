785f78e58e4a0d80daed9b0482deb4ef200725cc
pendi-kurnia <emailkamu@example.com>
2025-11-02 06:41:57 +0000

    menambahkan folder kampus dan 2 file.py.  init dan manifest

---
+++ addons/kampus/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Kampus',
+    'version': '1.0',
+    'summary': 'Modul uji coba untuk memastikan bisa terbaca di Odoo',
+    'author': 'Pendi Kurnia',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
